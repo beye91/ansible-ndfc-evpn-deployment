f071238a2fe0cee7481d92852ff445679a51878d
cbeye@cisco.com <cbeye@cisco.com>
2022-08-03 18:40:11 +0200

    Commit

--- dcnm-01_fabric-creation/change_variables.sh
+++ dcnm-01_fabric-creation/change_variables.sh
@@ -3,7 +3,9 @@
 while read serialNumber
 do 
     publicKey=`jq '.[] | select(.serialNumber == "'$serialNumber'") | {publicKey} ' /tmp/ansible_create_fabric/get_devices.output | grep publicKey | awk -F '"' '{print $4}'`
+    echo $publicKey
     fingerprint=`jq '.[] | select(.serialNumber == "'$serialNumber'") | {fingerprint} ' /tmp/ansible_create_fabric/get_devices.output | grep fingerprint | awk -F '"' '{print $4}'`
+    echo $fingerprint
     sed -e "s/publicKey_${serialNumber}/${publicKey}/" inventory.json
     sed -e "s/fingerprint_${serialNumber}/${fingerprint}/" inventory.json
 done < <(cat inventory.json | grep serialNumber | awk -F ':' '{print $2}')
