a1ce9291ae5d03620049262ec18cd5ce9bff8715
cbeye@cisco.com <cbeye@cisco.com>
2022-08-03 18:04:40 +0200

    Commit

--- dcnm-01_fabric-creation/change_variables.sh
+++ dcnm-01_fabric-creation/change_variables.sh
@@ -2,8 +2,10 @@
 
 while read serialNumber
 do 
-    publicKey=$(jq '.[] | select(.serialNumber == "'$serialNumber'") | {publicKey} ' /tmp/ansible_create_fabric/get_devices.output | grep publicKey | awk -F '"' '{print $2}')
-    fingerprint=$(jq '.[] | select(.serialNumber == "'$serialNumber'") | {fingerprint} ' /tmp/ansible_create_fabric/get_devices.output | grep fingerprint | awk -F '"' '{print $2}')
+    echo $serialNumber
+    publicKey=`jq '.[] | select(.serialNumber == "'$serialNumber'") | {publicKey} ' /tmp/ansible_create_fabric/get_devices.output | grep publicKey | awk -F '"' '{print $2}'`
+    fingerprint=`jq '.[] | select(.serialNumber == "'$serialNumber'") | {fingerprint} ' /tmp/ansible_create_fabric/get_devices.output | grep fingerprint | awk -F '"' '{print $2}'`
+    echo "$publicKey $fingerprint"
     sed -i "s/publicKey_'${serialNumber}'/'${publicKey}'/g" inventory.json
     sed -i "s/fingerprint_'${serialNumber}'/'${fingerprint}'/g" inventory.json
 done < <(cat inventory.json | grep serialNumber | awk -F ':' '{print $2}')
